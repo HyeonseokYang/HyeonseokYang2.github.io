ec4aeb0d6585bd776493248f29d9fa9df39d558c
HyeonseokYang <57899117+HyeonseokYang@users.noreply.github.com>
2021-03-25 16:04:55 +0900

    Create postmessage.js

---
+++ postmessage.js
@@ -0,0 +1,5 @@
+ 	window.addEventListener('message', function (e) {	
+ 		   document.querySelector("#txtNexonID").value="abc";
+ 		   document.querySelector("#txtPWD").value="123";
+ 		   document.querySelector("#btnLogin").click;
+ 		});
